9a542257bf7efe3595fe59bd07ae938947a87e50
Jayvardhan Rathi <39644109+ComputerScientist-01@users.noreply.github.com>
2020-07-06 10:19:29 +0000

    Add files via upload

---
+++ Minor Project/Weather Data/temperature.py
@@ -0,0 +1,11 @@
+import pyowm
+from api_key import API_KEY
+
+owm = pyowm.OWM(API_KEY)
+mgr = owm.weather_manager()
+obs = mgr.weather_at_place('San Francisco, US')
+weather = obs.weather
+
+
+temperature = weather.temperature(unit='celsius')
+print(f'The temperature in San Francisco, California is {temperature} degrees Celsius.')
